5aa9c11d0f9df54a199389e9f0d4ca725449a687
MaJunghan <wjdgksak@naver.com>
2020-10-31 22:25:14 +0900

    node기초

--- main.js
+++ main.js
@@ -5,34 +5,64 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/') {
-       // description이 <p>태그안의 값으로넘어감.
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-      <title>WEB1 - ${title} </title>
-      <meta charset="utf-8">
-      </head>
-      <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-          <li><a href="?id=HTML">HTML</a></li>
-          <li><a href="?id=CSS">CSS</a></li>
-          <li><a href="?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-      </body>
-      </html> 
-      `;
-      response.writeHead(200);
-      response.end(template);
-    });
+      // 사용자가 입력한 쿼리스트링 === undefind(정의되지않음)
+      if(queryData.id === undefined) {
+        // 쿼리스트링이 없다면 
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
+          var title = 'Welcom';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title} </title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+              <li><a href="?id=HTML">HTML</a></li>
+              <li><a href="?id=CSS">CSS</a></li>
+              <li><a href="?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+          </body>
+          </html> 
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+        // 받아온 쿼리스트링이있다면~~~~
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title} </title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+              <li><a href="?id=HTML">HTML</a></li>
+              <li><a href="?id=CSS">CSS</a></li>
+              <li><a href="?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+          </body>
+          </html> 
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
     } else{
       response.writeHead(404);
       response.end('not found');
